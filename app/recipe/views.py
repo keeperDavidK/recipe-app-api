2b9a1f882cda1197e4625304a39f5e1e00975343
David Kelly <david.kelly@keepersolutions.com>
2022-02-07 14:20:41 +0000

    added recipe detail view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -45,3 +45,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         '''return objects for logged in user only'''
         return self.queryset.filter(user=self.request.user).order_by('-title')
+
+    def get_serializer_class(self):
+        '''return appropriate'''
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+        
+        return self.serializer_class
