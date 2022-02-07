482ac65a75cd72553730e22950c8bb451d567d91
David Kelly <david.kelly@keepersolutions.com>
2022-02-07 09:54:50 +0000

    refactored tags and ingredients

--- app/core/models.py
+++ app/core/models.py
@@ -51,7 +51,8 @@ class Tag(models.Model):
 class Ingredient(models.Model):
     ''''''
     name = models.CharField(max_length=255)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
